ab5738b5b07f4fcb5c360dc9cf2ff51aec52c5d0
Jose Ruiz-Ramon <jose.pipo.ruiz@gmail.com>
2023-01-02 20:40:54 -0400

    Bug fixed, tailwind classes limited

--- src/components/ContainerMenu.js
+++ src/components/ContainerMenu.js
@@ -4,13 +4,13 @@ function ContainerMenu() {
 
       
       <div className="border-dotted container flex items-center justify-between mx-auto px-4 py-6 sm:px-6 lg:px-8">
-        <button id="aboutMeBttn" className="font-xbr menu-item text-lg font-medium leading-5 text-gray-900 hover:text-gray-700 transition duration-150 ease-in-out scale-80 hover:scale-150">
+        <button id="aboutMeBttn" className="font-xbr menu-item text-xl font-medium leading-5 text-gray-900 hover:text-gray-700">
           About Me
         </button>
-        <button id="projectsBttn" className="font-xbr menu-item text-lg font-medium leading-5 text-gray-900 hover:text-gray-700 transition duration-150 ease-in-out hover:scale-150">
+        <button id="projectsBttn" className="font-xbr menu-item text-xl font-medium leading-5 text-gray-900 hover:text-gray-700">
           Projects
         </button>
-        <button id="diaryBttn" className="font-xbr menu-item text-lg font-medium leading-5 text-gray-900 hover:text-gray-700 transition duration-150 ease-in-out hover:scale-150">
+        <button id="diaryBttn" className="font-xbr menu-item text-xl font-medium leading-5 text-gray-900 hover:text-gray-700">
           Diary
         </button>
       </div>
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,7 +1,7 @@
 
 function HomeHeader() {
     return (
-        <div className="bg-black ">
+        <div className="bg-black">
             <div className="flex  p-8 justify-center text-white">
                 <button className="font-kuusino text-6xl scale-90 hover:scale-150 ease-in duration-200">
                     JARrHUO
