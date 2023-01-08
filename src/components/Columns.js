40d5be5c842b22e084935e70754f988021c6b1df
Jose Ruiz-Ramon <jose.pipo.ruiz@gmail.com>
2023-01-08 02:43:05 -0400

    removed useless file and updated aesthetics

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -5,7 +5,7 @@ function Columns()
 
     return (
 
-        <div className=" h-screen rounded-lg grid grid-cols-2 gap-12 mx-6">
+        <div className=" h-screen rounded-lg grid grid-cols-2 gap-2 mx-6">
             <div className="">
                 <LastPostPreview
                     postType="diary"
--- src/components/LastPostPreview.js
+++ src/components/LastPostPreview.js
@@ -11,7 +11,7 @@ function LastPostPreview(props) {
 
   return (
     <div className="rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500 drop-shadow-2xl bg-gradient-to-r">
-      {posts ? <h1 className="text-white font-bold text-center text-xl">{posts[posts.length-1].header}</h1> : null}
+      {posts ? <h1 className="font-xbr text-white font-bold text-center text-2xl">{posts[posts.length-1].header}</h1> : null}
       {posts ? <h2 className="text-white italic text-center text-sm">{posts[posts.length-1].dateOfPost}</h2> : null}
       {posts ? <p className="px-2 py-2">{posts[posts.length-1].documentContent}</p> : null}
     </div>
